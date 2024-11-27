c7052b9945fc4345b20aa71a25373f9f27ecb361
ErfanCEP <rribasm2324@politecnics.barcelona>
2024-11-27 18:43:02 +0100

    log in fet
    index començat

--- index.php
+++ index.php
@@ -4,11 +4,73 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap.css">
+    <!-- <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap.css"> -->
+    <link rel="stylesheet" href="bootstrap-5.3.3/scss/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
+
 </head>
 
 <body>
+
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark">
+        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <strong>Nou projecte</strong>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <strong>Els meus projectes</strong>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <strong>Les meves tasques</strong>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+
+        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <strong>Comentaris</strong>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+
+
+
+    </div>
 
     <script src="bootstrap-5.3.3/dist/js/bootstrap.bundle.js"></script>
 </body>
--- log_in.php
+++ log_in.php
@@ -4,38 +4,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap.css">
+    <!-- <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap.css"> -->
+    <link rel="stylesheet" href="bootstrap-5.3.3/scss/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 
-    <style>
-        body {
-
-
-            background-color: orange;
-        }
-    </style>
 </head>
 
 <body>
-    <div class="container mt-10">
+    <div class="container bg-primary inici d-flex justify-content-center align-items-center vh-100 vw-100">
         <div class="row">
             <div class="col">
-                <img src="./img/P.gif"></img>
+                <img src="./img/logo_mov.gif"></img>
             </div>
-            <div class="col bg-primary">
-                <h2 class="fw-bold text-center py-5">Benvingut a Organizy</h2>
+            <div class="col bg-warning rounded-2">
+                <h2 class=" benvingut fw-bold text-center py-5 ps-5 pe-5 lletra_blanca">BENVINGUT A ORGANIZY</h2>
                 <form action="">
                     <div class="mb-4">
-                        <label for="nom" class="form-label">Nom</label>
+                        <label for="nom" class="form-label lletra_blanca ">Nom</label>
                         <input type="text" class="form-control">
                     </div>
                     <div class="mb-4">
-                        <label for="nom" class="form-label">Contrasenya</label>
+                        <label for="nom" class="form-label lletra_blanca">Contrasenya</label>
                         <input type="password" class="form-control">
                     </div>
                     <div class="d-grid">
-                        <button type="submit" class="btn btn-primary">Entrar</button>
-                        <button type="submit" class="btn btn-secondary my-1">Registrar-se</button>
+                        <button type="submit" class="btn btn-info my-4">Entrar</button>
+                        <button type="submit" class="btn btn-secondary ">Registrar-se</button>
                     </div>
 
 
